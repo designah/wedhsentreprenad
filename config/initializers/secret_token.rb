005da7fdb0ab3115c64447c0af574dc37f7bd1d2
markus <markusdevelopment@gmail.com>
2012-07-23 18:41:28 +0200

    Initilize newest version, a lot of styling left

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedhsentreprenad::Application.config.secret_token = 'a98e68463db4f29d1ed35f45c7ebedaf8946c48ce898ed303ca2f89451986fb6ec5a30f4f0a3c58c9a06299b66a34efad173c8a4adda39664308ef17ccd8db62'
